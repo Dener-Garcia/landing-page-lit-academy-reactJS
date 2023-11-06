9c287dcd153b414cb5f96f8666abc13e4716962a
Dener Garcia <dener.criarbr.com>
2023-11-06 00:00:53 -0300

    fix: call api test1

--- src/api/fetchTestimonials.js
+++ src/api/fetchTestimonials.js
@@ -1,6 +1,6 @@
 const fetchTestimonials = async () => {
   const testimonials = await fetch('src/api/testimonials.json');
-  const data = await testimonials.json();
+  const data = await testimonials.JSON();
   return data.testimonials;
 };
 
